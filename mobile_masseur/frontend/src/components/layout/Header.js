5b3f4b85f3db7e7cdd6e2326781a2a059da64cd8
Adam Antkowiak <adam.antkowiak93@gmail.com>
2020-10-03 21:33:21 +0200

    Initial commit

---
+++ mobile_masseur/frontend/src/components/layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <div className="logo">BRAND LOGO</div>
+                <ul>
+                    <li>xd</li>
+                    <li>xd</li>
+                    <li>xd</li>
+                    <li>xd</li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Header
