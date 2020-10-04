a5295f2ba5dfe4f88251341d1481e37c6ca1df8f
Paulina Grajewska <paulina_darcy@o2.pl>
2020-10-04 10:52:35 +0200

    Add phonenumber field in model User

--- mobile_masseur/mobile_masseur/settings.py
+++ mobile_masseur/mobile_masseur/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'rest_framework',
-    'frontend'
+    'frontend',
+    'phonenumber_field'
 ]
 
 MIDDLEWARE = [
--- mobile_masseur/users/models.py
+++ mobile_masseur/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(models.Model):
@@ -6,3 +7,11 @@ class User(models.Model):
     email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    phone = PhoneNumberField(default='+48000000000')
+
+    class Meta:
+        unique_together = ('name', 'phone')
+        ordering = ('name',)
+
+    def __str__(self):
+        return f"{self.name} email: {self.email}"
