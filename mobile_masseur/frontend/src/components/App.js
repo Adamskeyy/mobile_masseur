1eeeabba02ae7696702073dbaf0d6bbd1b7c50a0
Adam Antkowiak <adam.antkowiak93@gmail.com>
2020-10-07 01:04:21 +0200

    Dummy

--- mobile_masseur/frontend/src/components/App.js
+++ mobile_masseur/frontend/src/components/App.js
@@ -5,7 +5,12 @@ import Header from './layout/Header';
 class App extends Component {
     render() {
         return (
-            <Header />
+            <div>
+                <Header />
+                <h1>MOBILNY MASAŻYSTA WOHOOO!!!!</h1>
+                <img style={{ width: '600px' }} src="https://ocdn.eu/images/pulscms/YTM7MDA_/237b4f5678a36c96a3258215f8c93df2.jpg" alt="STONOGA" />
+            </div>
+
         )
     }
 }
