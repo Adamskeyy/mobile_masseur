08e7e5a96f3f79477a1f1c8b1d7efd3def7de33c
Paulina Grajewska <paulina_darcy@o2.pl>
2020-10-06 22:15:31 +0200

    Alter models MassageService (choices for free dates for service)

--- mobile_masseur/massage/models.py
+++ mobile_masseur/massage/models.py
@@ -11,14 +11,6 @@ class MassageDateTime(models.Model):
         return f"{self.day} - {self.hour}"
 
 
-b = MassageDateTime(day="2020-11-11", hour="12:00")
-a = MassageDateTime(day="2020-11-11", hour="15:00")
-FREE_DATES = Choices(
-    (1, f"{a.day} + {a.hour}", f"{a.day} {a.hour}"),
-    (2, f"{b.day} + {b.hour}", f"{b.day} {b.hour}"),
-)
-
-
 class MassageDelivery(models.Model):
     place = models.CharField(max_length=100)
     cost = models.IntegerField(default=0)
@@ -34,20 +26,30 @@ class MassageType(models.Model):
     cost = models.IntegerField()
 
     def __str__(self):
-        return f"{self.name} - {self.duration} min - {self.cost} zł"
+        return f"{self.name} - {self.duration} min"
 
     class Meta:
-        unique_together = ('name', 'duration', 'cost')
+        unique_together = ('name', 'duration')
         ordering = ('name',)
 
 
 class MassageService(models.Model):
-    # FREE_DATES = [(x, x) for x in MassageDateTime.objects.all()]
     massage_type = models.ForeignKey(MassageType, on_delete=models.DO_NOTHING)
-    massage_date = models.IntegerField(choices=FREE_DATES)
+    massage_date_time = models.CharField(
+        max_length=200,
+        choices=[(str(free_date), str(free_date)) for free_date in MassageDateTime.objects.all()],
+        default="-"
+    )
     comment = models.TextField(null=True, blank=True)
     massage_delivery = models.ForeignKey(MassageDelivery, on_delete=models.DO_NOTHING)
     address = models.CharField(max_length=200, blank=True, null=True)
 
+    class Meta:
+        unique_together = ('massage_date_time',)
+        ordering = ('massage_date_time',)
+
     def total_cost(self):
         return self.massage_type.cost + self.massage_delivery.cost
+
+    def __str__(self):
+        return f"{self.massage_type} / {self.massage_date} / {self.total_cost()} zł"
